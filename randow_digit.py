d0f1e8c4bb2492c85a3473fedb31161de899775a
nchvi <81639112+nchvi@users.noreply.github.com>
2022-04-25 19:36:25 +0300

    Update randow_digit.py

--- randow_digit.py
+++ randow_digit.py
@@ -1,35 +1,63 @@
 
-### Добавьте возможность генерации нового числа (повторная игра), после того, как пользователь угадал число;
-### Добавить возможность указания правой границы для случайного выбора числа (от 111 до nnn).
+### Добавить возможность указания правой границы для случайного выбора числа (от 111 до nnn). - не в цикле
+
 
 from random import *
-n = randint(1, 100)
+from time import sleep
+
 print('Добро пожаловать в числовую угадайку')
+sleep(1)
+print('Выберете диапазон значений, в котором будете угадывать числа')
+sleep(1)
+start_range = int(input('Введите начальное значение:   '))
+end_range = int(input('Введите конечное значение:   '))
+if start_range > end_range:
+    start_range, end_range = end_range, start_range
 
 def check(s):
     while True:
-        if s.isdigit() and 1 <= int(s) <= 100:
+        if s.isdigit() and start_range <= int(s) <= end_range:
             s = int(s)
             break
         else:
-            print('А может быть все-таки введем целое число от 1 до 100?')
-            s = input('Введите число от 1 до 100:  ')
+            print('А может быть все-таки введем целое число от', start_range, 'до',  end_range, '?')
+            s = input()
     return s
 
 
-s = check(input('Введите число от 1 до 100:  '))
-counter = 1
-while s != n:
-    if s < n:
-        print('Ваше число меньше загаданного, попробуйте еще разок')
+def game():
+    while True:
+        n = randint(start_range, end_range)
+        print('Введите число от', start_range, 'до',  end_range)
         s = check(input())
-        counter += 1
-        continue
-    elif s > n:
-        print('Ваше число больше загаданного, попробуйте еще разок')
-        s = check(input())
-        counter += 1
-        continue
-print('Вы угадали, поздравляем!, вам потребовалось', counter, 'попыток, чтобы угадать число!')
-counter = 1
-print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+        counter = 1
+        while s != n:
+            if s < n:
+                print('Ваше число меньше загаданного, попробуйте еще разок')
+                s = check(input())
+                counter += 1
+                continue
+            elif s > n:
+                print('Ваше число больше загаданного, попробуйте еще разок')
+                s = check(input())
+                counter += 1
+                continue
+        print('Вы угадали, поздравляем!, вам потребовалось', counter, 'попыток, чтобы угадать число!')
+        counter = 1
+        sleep(1)
+        new_game = input('Хотите сыграть еще раз? Введите "Да" или "Нет":  ').lower()
+        while True:
+            if new_game == 'да' or new_game == 'нет':
+                break
+            else:
+                new_game = input('Введите "Да" или "Нет":  ')
+                sleep(1)
+        if new_game == 'нет':
+            print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+            break
+        else:
+            continue
+
+game()
+
+
